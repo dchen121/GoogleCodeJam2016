7f2594c59f05be7a8dc6b20a0b4dbbe40882ec04
Daniel Chen <chen.d__@hotmail.com>
2016-04-09 21:56:35 -0700

    added problem details, refactored

--- src/Qualification/CountingSheep.java
+++ src/Qualification/CountingSheep.java
@@ -5,9 +5,56 @@ import java.io.*;
 
 public class CountingSheep {
 
+    /**
+     * Problem A. Counting Sheep
+     *
+     * Problem
+     *
+     * Bleatrix Trotter the sheep has devised a strategy that helps her fall asleep faster. First, she picks a number N.
+     * Then she starts naming N, 2 × N, 3 × N, and so on. Whenever she names a number, she thinks about all of the
+     * digits in that number. She keeps track of which digits (0, 1, 2, 3, 4, 5, 6, 7, 8, and 9) she has seen at least
+     * once so far as part of any number she has named. Once she has seen each of the ten digits at least once, she will
+     * fall asleep.
+     *
+     * Bleatrix must start with N and must always name (i + 1) × N directly after i × N. For example, suppose that
+     * Bleatrix picks N = 1692. She would count as follows:
+     *      N = 1692. Now she has seen the digits 1, 2, 6, and 9.
+     *      2N = 3384. Now she has seen the digits 1, 2, 3, 4, 6, 8, and 9.
+     *      3N = 5076. Now she has seen all ten digits, and falls asleep.
+     *
+     * What is the last number that she will name before falling asleep? If she will count forever, print INSOMNIA instead.
+     *
+     * Input
+     * The first line of the input gives the number of test cases, T. T test cases follow. Each consists of one line
+     * with a single integer N, the number Bleatrix has chosen.
+     *
+     * Output
+     * For each test case, output one line containing Case #x: y, where x is the test case number (starting from 1) and
+     * y is the last number that Bleatrix will name before falling asleep, according to the rules described in the statement.
+     *
+     * Limits
+     * 1 ≤ T ≤ 100.
+     *
+     * Small dataset
+     * 0 ≤ N ≤ 200.
+     *
+     * Large dataset
+     * 0 ≤ N ≤ 106.
+     *
+     * Sample
+     * Input    Output
+     * 5        Case #1: INSOMNIA
+     * 0        Case #2: 10
+     * 1        Case #3: 90
+     * 2        Case #4: 110
+     * 11       Case #5: 5076
+     * 1692
+     *
+     */
+
     public static void main(String[] args) {
         Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
-        int t = in.nextInt();  // Scanner has functions to read ints, longs, strings, chars, etc.
+        int t = in.nextInt();
         for (int i = 1; i <= t; ++i) {
             int n = in.nextInt();
 
@@ -17,7 +64,13 @@ public class CountingSheep {
         }
     }
 
-    private static Object insomnia(int n) {
+    /**
+     * Return the last number Bleatrix will name before falling asleep. If she will count forever, print INSOMNIA.
+     *
+     * @param n
+     * @return
+     */
+    private static String insomnia(int n) {
         int[] a = new int[10];
         int currNum;
         int tempNum;
@@ -26,19 +79,26 @@ public class CountingSheep {
             currNum = i*n;
             tempNum = currNum;
 
+            // Record each digit encountered
             while (tempNum > 0) {
                 a[tempNum%10] = 1;
                 tempNum = tempNum/10;
             }
 
             if (containsAllOnes(a)) {
-                return currNum;
+                return String.valueOf(currNum);
             }
         }
 
         return "INSOMNIA";
     }
 
+    /**
+     * Returns true if the integer array contains only 1 for each index.
+     *
+     * @param a
+     * @return
+     */
     private static boolean containsAllOnes(int[] a) {
         for (int i = 0; i < a.length; i++) {
             if (a[i] != 1) {
--- src/Qualification/RevengeOfThePancakes.java
+++ src/Qualification/RevengeOfThePancakes.java
@@ -3,23 +3,88 @@ package Qualification;
 import java.util.*;
 import java.io.*;
 
-/**
- * Created by Daniel on 2016-04-09.
- */
 public class RevengeOfThePancakes {
+
+    /**
+     * Problem B. Revenge of the Pancakes
+     *
+     * Problem
+     * The Infinite House of Pancakes has just introduced a new kind of pancake! It has a happy face made of chocolate
+     * chips on one side (the "happy side"), and nothing on the other side (the "blank side").
+     *
+     * You are the head waiter on duty, and the kitchen has just given you a stack of pancakes to serve to a customer.
+     * Like any good pancake server, you have X-ray pancake vision, and you can see whether each pancake in the stack
+     * has the happy side up or the blank side up. You think the customer will be happiest if every pancake is happy
+     * side up when you serve them.
+     *
+     * You know the following maneuver: carefully lift up some number of pancakes (possibly all of them) from the top of
+     * the stack, flip that entire group over, and then put the group back down on top of any pancakes that you did not
+     * lift up. When flipping a group of pancakes, you flip the entire group in one motion; you do not individually flip
+     * each pancake. Formally: if we number the pancakes 1, 2, ..., N from top to bottom, you choose the top i pancakes
+     * to flip. Then, after the flip, the stack is i, i-1, ..., 2, 1, i+1, i+2, ..., N. Pancakes 1, 2, ..., i now have
+     * the opposite side up, whereas pancakes i+1, i+2, ..., N have the same side up that they had up before.
+     *
+     * For example, let's denote the happy side as + and the blank side as -. Suppose that the stack, starting from the
+     * top, is --+-. One valid way to execute the maneuver would be to pick up the top three, flip the entire group, and
+     * put them back down on the remaining fourth pancake (which would stay where it is and remain unchanged). The new
+     * state of the stack would then be -++-. The other valid ways would be to pick up and flip the top one, the top
+     * two, or all four. It would not be valid to choose and flip the middle two or the bottom one, for example; you can
+     * only take some number off the top.
+     *
+     * You will not serve the customer until every pancake is happy side up, but you don't want the pancakes to get
+     * cold, so you have to act fast! What is the smallest number of times you will need to execute the maneuver to get
+     * all the pancakes happy side up, if you make optimal choices?
+     *
+     * Input
+     * The first line of the input gives the number of test cases, T. T test cases follow. Each consists of one line
+     * with a string S, each character of which is either + (which represents a pancake that is initially happy side up)
+     * or - (which represents a pancake that is initially blank side up). The string, when read left to right,
+     * represents the stack when viewed from top to bottom.
+     *
+     * Output
+     * For each test case, output one line containing Case #x: y, where x is the test case number (starting from 1) and
+     * y is the minimum number of times you will need to execute the maneuver to get all the pancakes happy side up.
+     *
+     * Limits
+     * 1 ≤ T ≤ 100.
+     * Every character in S is either + or -.
+     *
+     * Small dataset
+     * 1 ≤ length of S ≤ 10.
+     *
+     * Large dataset
+     * 1 ≤ length of S ≤ 100.
+     *
+     * Sample
+     * Input    Output
+     * 5        Case #1: 1
+     * -        Case #2: 1
+     * -+       Case #3: 2
+     * +-       Case #4: 0
+     * +++      Case #5: 3
+     * --+-
+     *
+     */
 
     public static void main(String[] args) {
         Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
-        int t = in.nextInt();  // Scanner has functions to read ints, longs, strings, chars, etc.
+        int t = in.nextInt();
         in.nextLine();
         for (int i = 1; i <= t; ++i) {
             String s = in.nextLine();
-            int flips = countFlipsPlus(s, 0);
+            int flips = countFlipsHappySideUp(s, 0);
             System.out.println("Case #" + i + ": " + (flips));
         }
     }
 
-    private static int countFlipsPlus(String s, int count) {
+    /**
+     * Counts the minimum number of flips required such that every pancake is happy side up.
+     *
+     * @param s
+     * @param count
+     * @return
+     */
+    private static int countFlipsHappySideUp(String s, int count) {
         if (s.length() == 0) {
             return count;
         }
@@ -32,25 +97,32 @@ public class RevengeOfThePancakes {
             }
         }
 
-        else if (allDash(s)) {
+        else if (allHappySideDown(s)) {
             return count+1;
         }
 
         else if (s.substring(s.length()-1,s.length()).equals("+")) {
-            return countFlipsPlus(s.substring(0, s.length()-1), count);
+            return countFlipsHappySideUp(s.substring(0, s.length()-1), count);
         }
 
         else if (s.substring(0, 1).equals("-") && s.substring(s.length()-1, s.length()).equals("-")) {
-            String newStr = reverseAndFlip(s);
-            return countFlipsPlus(newStr, count+1);
+            String newStr = flipPancakes(s);
+            return countFlipsHappySideUp(newStr, count+1);
         }
 
         else {
-            return 1 + countFlipsDash(s, count);
+            return 1 + countFlipsHappySideDown(s, count);
         }
     }
 
-    private static int countFlipsDash(String s, int count) {
+    /**
+     * Counts the minimum number of flips required such that every pancake is unhappy side up.
+     *
+     * @param s
+     * @param count
+     * @return
+     */
+    private static int countFlipsHappySideDown(String s, int count) {
         if (s.length() == 0) {
             return count;
         }
@@ -63,25 +135,31 @@ public class RevengeOfThePancakes {
             }
         }
 
-        else if (allPlus(s)) {
+        else if (allHappySideUp(s)) {
             return count+1;
         }
 
         else if (s.substring(s.length()-1,s.length()).equals("-")) {
-            return countFlipsDash(s.substring(0, s.length()-1), count);
+            return countFlipsHappySideDown(s.substring(0, s.length()-1), count);
         }
 
         else if (s.substring(0, 1).equals("+") && s.substring(s.length()-1, s.length()).equals("+")) {
-            String newStr = reverseAndFlip(s);
-            return countFlipsDash(newStr, count+1);
+            String newStr = flipPancakes(s);
+            return countFlipsHappySideDown(newStr, count+1);
         }
 
         else {
-            return 1 + countFlipsPlus(s, count);
+            return 1 + countFlipsHappySideUp(s, count);
         }
     }
 
-    private static boolean allPlus(String s) {
+    /**
+     * Returns true if all pancakes are happy side up, false otherwise.
+     *
+     * @param s
+     * @return
+     */
+    private static boolean allHappySideUp(String s) {
         for (int i = 0; i < s.length(); i++) {
             if (!s.substring(i, i+1).equals("+")) {
                 return false;
@@ -91,7 +169,13 @@ public class RevengeOfThePancakes {
         return true;
     }
 
-    private static boolean allDash(String s) {
+    /**
+     * Returns true if all pancakes are happy side down, false otherwise.
+     *
+     * @param s
+     * @return
+     */
+    private static boolean allHappySideDown(String s) {
         for (int i = 0; i < s.length(); i++) {
             if (!s.substring(i, i+1).equals("-")) {
                 return false;
@@ -101,7 +185,13 @@ public class RevengeOfThePancakes {
         return true;
     }
 
-    private static String reverseAndFlip(String s) {
+    /**
+     * Flips the stack of pancakes over.
+     *
+     * @param s
+     * @return
+     */
+    private static String flipPancakes(String s) {
         String newStr = new StringBuilder(s).reverse().toString();
         newStr = newStr.replace("+", "1");
         newStr = newStr.replace("-", "+");
